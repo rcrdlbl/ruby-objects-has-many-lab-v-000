f11a15325e7d1de2e5e577b4f2f9cba169df05aa
Przemyslaw Dabrowski <bluh@outlook.com>
2018-08-10 02:27:05 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,25 @@
-class Author
+class Artist
+  attr_accessor :name, :posts
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def add_post(post)
+    song.artist = self
+    @posts << post
+  end
+
+  def add_title_by_title(title)
+    post = Post.new(title)
+    post.author = self
+    @posts << post
+  end
+
+  def self.song_count
+    return Song.all.count
+  end
+
 
 end
